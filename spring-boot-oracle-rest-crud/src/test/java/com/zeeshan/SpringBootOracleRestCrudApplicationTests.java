d38480bdc16f128c01445b4ac2bb07c163b61018
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-02-04 02:42:44 +0530

    Spring boot Application class

---
+++ spring-boot-oracle-rest-crud/src/test/java/com/zeeshan/SpringBootOracleRestCrudApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zeeshan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootOracleRestCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
