2c6c28e2880168d938fa399347b744abe42bdab5
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-02-08 23:30:17 +0530

    preparing our application to be run on external tomcat server

--- spring-boot-oracle-rest-crud/src/main/java/com/zeeshan/SpringBootOracleRestCrudApplication.java
+++ spring-boot-oracle-rest-crud/src/main/java/com/zeeshan/SpringBootOracleRestCrudApplication.java
@@ -2,9 +2,11 @@ package com.zeeshan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootOracleRestCrudApplication {
+public class SpringBootOracleRestCrudApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOracleRestCrudApplication.class, args);
