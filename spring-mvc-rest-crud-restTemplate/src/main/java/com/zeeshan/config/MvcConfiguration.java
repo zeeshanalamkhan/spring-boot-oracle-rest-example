17bbeae9b526ae1af63af701de5214cb9d6406e4
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-05-02 13:41:37 +0530

    SpringMVC-REST example

---
+++ spring-mvc-rest-crud-restTemplate/src/main/java/com/zeeshan/config/MvcConfiguration.java
@@ -0,0 +1,12 @@
+package com.zeeshan.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.zeeshan")
+public class MvcConfiguration {
+
+}
---
+++ spring-mvc-rest-crud-restTemplate/src/main/java/com/zeeshan/config/ServletInitializer.java
@@ -0,0 +1,31 @@
+package com.zeeshan.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { MvcConfiguration.class };
+
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+
+		Filter[] singleton = { new CORSFilter() };
+		return singleton;
+	}
+}
