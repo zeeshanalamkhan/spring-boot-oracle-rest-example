51cb581be4cfd2a7780c0fe639cbf61273b627d3
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-05-02 12:22:03 +0530

    Json to Object and vice versa class

---
+++ spring-boot-rest-api-consumer/src/main/java/com/zeeshan/util/JsonUtil.java
@@ -0,0 +1,41 @@
+package com.zeeshan.util;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class JsonUtil {
+
+	public static String javaToJson(Object obj) {
+		String jsonStr = "{}";
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			jsonStr = mapper.writeValueAsString(obj);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return jsonStr;
+	}
+
+	public static <T> T jsonToJava(Class<T> cls, String str) {
+		T response = null;
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			response = mapper.readValue(str, cls);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+}
